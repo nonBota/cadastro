de899ac24f5ae8a942be9bbdc4bd6447c42030f8
Srbottine <45329014+Srbottine@users.noreply.github.com>
2019-09-25 11:49:03 -0300

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,81 @@
+<?php
+    
+    $servidor = 'redmine.senailondrina.com';
+
+    $usuario = 'istldn';
+
+    $senha = 'txjXj7F6d5Frzeyvpatg';
+
+    $banco = 'ISTLDN';
+
+    $mysqli = new mysqli($servidor, $usuario, $senha, $banco);
+
+    if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
+
+    if($mysqli === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+
+    $cod_contrato = mysqli_real_escape_string($mysqli, $_REQUEST['cod_contrato']);
+    $tip_faturamento = mysqli_real_escape_string($mysqli, $_REQUEST['tip_faturamento']);
+    $crm_proposta = mysqli_real_escape_string($mysqli, $_REQUEST['crm_proposta']);
+    $cnpj_contratante = mysqli_real_escape_string($mysqli, $_REQUEST['cnpj_contratante']);
+    $raz_contratante = mysqli_real_escape_string($mysqli, $_REQUEST['raz_contratante']);
+    $emp_atendida = mysqli_real_escape_string($mysqli, $_REQUEST['emp_atendida']);
+    $num_servico = mysqli_real_escape_string($mysqli, $_REQUEST['num_servico']);
+    $cod_modalidade = mysqli_real_escape_string($mysqli, $_REQUEST['cod_modalidade']);
+    $cod_reduzido = mysqli_real_escape_string($mysqli, $_REQUEST['cod_reduzido']);
+    $des_servico =  mysqli_real_escape_string($mysqli, $_REQUEST['des_servico']);
+    $cod_consultor =  mysqli_real_escape_string($mysqli, $_REQUEST['cod_consultor']);
+    $hor_servico = mysqli_real_escape_string($mysqli, $_REQUEST['hor_servico']);
+    $dat_inicio = mysqli_real_escape_string($mysqli, $_REQUEST['dat_inicio']);
+    $dat_termino = mysqli_real_escape_string($mysqli, $_REQUEST['dat_termino']);
+    $sts_contrato = mysqli_real_escape_string($mysqli, $_REQUEST['sts_contrato']);
+    $vlr_contrato = mysqli_real_escape_string($mysqli, $_REQUEST['vlr_contrato']);
+
+    $sql = "INSERT INTO contratos(
+              `cod_contrato`, 
+              `tip_faturamento`, 
+              `crm_proposta`, 
+              `cnpj_contratante`, 
+              `raz_contratante`, 
+              `emp_atendida`, 
+              `num_servico`, 
+              `cod_modalidade`, 
+              `cod_reduzido`, 
+              `des_servico`, 
+              `cod_consultor`, 
+              `hor_servico`,  
+              `dat_inicio`, 
+              `dat_termino`, 
+              `sts_contrato`, 
+              `vlr_contrato`
+            )
+     VALUES (
+             '".$cod_contrato."', 
+             '".$tip_faturamento."', 
+             '".$crm_proposta."', 
+             '".$cnpj_contratante."', 
+             '".$raz_contratante."', 
+             '".$emp_atendida."', 
+             '".$num_servico."', 
+             '".$cod_modalidade."', 
+             '".$cod_reduzido."',   
+             '".$des_servico."', 
+             '".$cod_consultor."', 
+             '".$hor_servico."',
+             '".$dat_inicio."', 
+             '".$dat_termino."', 
+             '".$sts_contrato."', 
+             '".$vlr_contrato."'
+      )";
+
+    if(mysqli_query($mysqli, $sql)){
+        echo "Records added successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+    }
+
+$var = "<script>javascript:history.back(-2)</script>";
+echo $var;
+?>
---
+++ forms.php
@@ -0,0 +1,134 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro</title>
+
+
+</head>
+<body>
+<style>
+    input{
+        -webkit-box-sizing: content-box;
+        -moz-box-sizing: content-box;
+        box-sizing: content-box;
+        z-index: auto;
+        width: auto;
+        height: auto;
+        position: static;
+        cursor: default;
+        opacity: 1;
+        margin: 0;
+        padding: 10px 20px;
+        overflow: visible;
+        border: 1px solid #b7b7b7;
+        -webkit-border-radius: 6px;
+        border-radius: 6px;
+        font: normal 12px/normal Arial, Helvetica, sans-serif;
+        -o-text-overflow: clip;
+        text-overflow: clip;
+        -webkit-transform-origin: 50% 50% 0;
+        transform-origin: 50% 50% 0;
+    }
+   label{
+        display: inline-block;
+        width:100%;       
+   }
+   table{
+       width: 100%;
+       color: #467aa7;
+       font-family: Arial;
+   }
+</style>
+
+    <h2>Cadastro</h2>
+    <div>
+    <form class="table border" action="conn.php" metrod="POST">
+        
+   <table>
+       <tr>
+           <td>
+                <label for="cod_contrato">Código do Contrato:</label>
+                <input value="1" type="text" id="cod_contrato" name="cod_contrato">
+           </td>
+           <td>
+                <label for="tip_faturamento">Tipo faturamento:</label>
+                <input value="2" type="number" id="tip_faturamento" name="tip_faturamento">
+            </td>
+            <td>
+                <label for="crm_proposta">Crm proposta:</label>
+                <input value="3" type="text" id="crm_proposta" name="crm_proposta">
+            </td>
+            <td>
+                <label for="cnpj_contratante">CNPJ:</label>
+                <input value="4" type="text" id="cnpj_contratante" name="cnpj_contratante">
+            </td>
+            </tr>
+            <tr>
+            <td>
+                <label for="raz_contratante">Razão:</label>
+                <input value="5" type="text" id="raz_contratante" name="raz_contratante">
+            </td>
+            <td>
+                <label for="emp_atendida">Empresa:</label>
+                <input value="6" type="text" id="emp_atendida" name="emp_atendida">
+            </td>
+            <td>
+                <label for="num_servico">Número de Servico:</label>
+                <input value="7" type="number" id="num_servico" name="num_servico">
+           </td>
+           <td>    
+                <label for="cod_modalidade">Código da Modalidade:</label>
+                <input value="8" type="number" id="cod_modalidade" name="cod_modalidade">
+           </td>
+        </tr>
+       <tr>
+           <td>
+                <label for="cod_reduzido">Código Reduzido:</label>
+                <input value="9" type="number" id="cod_reduzido" name="cod_reduzido"> 
+           </td>
+           <td>   
+                <label for="des_servico">Descrição do Servico:</label>
+                <input value="10" type="text" id="des_servico" name="des_servico">
+           </td>
+           <td>
+                <label for="cod_consultor">Código do(a) Colsultor(a):</label>
+                <input value="11" type="number" id="cod_consultor" name="cod_consultor">
+           </td>
+           <td>
+                <label for="hor_servico">Horario de Serviço:</label>
+                <input value="12.12" type="text" id="hor_servico" name="hor_servico">
+           </td>
+       </tr>
+       <tr>
+           <td>
+                <label for="dat_inicio">Data de Inicio:</label>
+                <input value="2019-09-23" type="date" id="dat_inicio" name="dat_inicio">    
+           </td>
+           <td>
+                <label for="dat_termino">Data de Término:</label>
+                <input value="2019-09-23" type="date" id="dat_termino" name="dat_termino">
+           </td>
+           <td>
+                <label for="sts_contrato">Sts contrato:</label>
+                <input value="15" type="number" id="sts_contrato" name="sts_contrato">
+           </td>
+           <td>
+                <label for="vlr_contrato">Valor do Contrato:</label>
+                <input value="16.16" type="text" id="vlr_contrato" name="vlr_contrato">
+           </td>
+       </tr>
+       <tr>
+            <td>
+                <input type="submit">
+            </td>
+       </tr>
+   </table> 
+    </form>
+</div>
+
+
+<h2>    Parcelas     </h2>
+
+
+</body>
+</html>
